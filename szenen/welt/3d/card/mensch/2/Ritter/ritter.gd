2f57a505ebbae1b33ec50dcb12b1f527b1e47d8b
Sean Patrick Gebor <sean.gebor@AD.B-TS-EKOS.LOGODIDACT.NET>
2025-02-24 13:27:37 +0100

    added 2d window on card click

--- szenen/welt/3d/card/mensch/2/Ritter/ritter.gd
+++ szenen/welt/3d/card/mensch/2/Ritter/ritter.gd
@@ -3,7 +3,13 @@ extends "res://szenen/welt/3d/card/mensch/1/mensch1.gd"
 
 func _ready():
 	angriff = 1  
-	leben = 2  
+	leben = 2
+	headclass = "Mensch"
+	subclass = "Ritter"
+	rating = 2
 	add_to_group("Mensch")
 	add_to_group("Ritter")  
 	update_labels()
+	cardmenu_path = "res://szenen/welt/3d/card/" + str(headclass.to_lower()) + "/" + str(rating) + "/" + str(self.name).to_lower() + "/cardmenu.tscn"
+	print("path for" + str(self.name) + "is: " + str(cardmenu_path))
+	cardmenu = load(cardmenu_path)
