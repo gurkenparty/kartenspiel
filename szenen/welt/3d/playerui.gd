d75015e3a3a2e188d8d1e805b8f420548cac9d98
Sean Patrick Gebor <sean.gebor@AD.B-TS-EKOS.LOGODIDACT.NET>
2025-03-03 12:47:27 +0100

    added material UI

--- szenen/welt/3d/playerui.gd
+++ szenen/welt/3d/playerui.gd
@@ -1,10 +1,10 @@
 extends Control
 
-@onready var holz_label = $HBoxContainer/Holz
-@onready var stein_label = $HBoxContainer/Stein
-@onready var metall_label = $HBoxContainer/Metall
-@onready var amethyst_label = $HBoxContainer/Amethyst
-@onready var gold_label = $HBoxContainer/Gold
+@onready var holz_label = $Holz
+@onready var stein_label = $Stein
+@onready var metall_label = $Metall
+@onready var amethyst_label = $Amethyst
+@onready var gold_label = $Gold
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -12,8 +12,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	holz_label.text = "Holz: " + str(GameState.ressources["Holz"])
-	stein_label.text = "Stein: " + str(GameState.ressources["Stein"])
-	metall_label.text = "Metall: " + str(GameState.ressources["Metall"])
-	amethyst_label.text = "Amethyst: " + str(GameState.ressources["Amethyst"])
-	gold_label.text = "Gold: " + str(GameState.ressources["Gold"])
+	holz_label.text =  str(GameState.ressources["Holz"])
+	stein_label.text =  str(GameState.ressources["Stein"])
+	metall_label.text =   str(GameState.ressources["Metall"])
+	amethyst_label.text =  str(GameState.ressources["Amethyst"])
+	gold_label.text = str(GameState.ressources["Gold"])
