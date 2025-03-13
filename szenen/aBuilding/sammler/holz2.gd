c752eb2ffff64268744e60a10d0ef283f0f2594f
max <maximilian.turin@AD.B-TS-EKOS.LOGODIDACT.NET>
2025-03-13 09:36:12 +0100

    Sammler

---
+++ szenen/aBuilding/sammler/holz2.gd
@@ -0,0 +1,18 @@
+extends "res://szenen/aBuilding/gebmain.gd"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	cost = {"Holz":2, "Stein":0, "Metall":0, "Amethyst":0}
+	leben = 8
+	headclass = "Gebaude"
+	subclass = "Holz"
+	bring_ressources = {"Holz" : 2, "Stein" : 0, "Metall" : 0, "Amethyst" : 0, "Gold" : 0}
+	
+	add_to_group("Gebaude")
+	add_to_group("Holz")
+	update_labels()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- szenen/deck.gd
+++ szenen/deck.gd
@@ -7,7 +7,14 @@ var deck = [
 	"Graf_Zacharias", "Gerd", "Urus", "Bär",
 	"Gift", "Späher", "Muti", "Stratege", "Meister",
 	"Unge", "Hauer", "Zerfleischer", "EF", "RO", "Kirill", "Blumen", "Erde", 
-	"Feuer", "Glas" , "Licht", "Natur", "Schatten", "Stahl", "Wasser", "Xera", "TW", "BB", "EK", "Vet", "Sch"
+	"Feuer", "Glas" , "Licht", "Natur", "Schatten", "Stahl", "Wasser", "Xera", "TW", 
+	"BB", "EK", "Vet", "Sch", "Schmied", "Erdbro", "Qualle", "Falle", 
+	"Holz1", "Holz2", "Holz3", "Holz4",
+	"Stein1", "Stein2", "Stein3", "Stein4",
+	"Metall1", "Metall2", "Metall3", "Metall4",
+	"Amethyst1", "Amethyst2", "Amethyst3", "Amethyst4",
+
+	
 ]  # Stores the card names
 
 func _ready():
